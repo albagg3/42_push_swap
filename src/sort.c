268137668a5c83f2b171f6c4b6b4272d4004053d
albagg3 <garciagonzalezalba@gmail.com>
2022-12-21 12:32:15 +0100

    ordena 3 4 5

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: albagarc <albagarc@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:58:33 by albagarc          #+#    #+#             */
-/*   Updated: 2022/12/21 12:08:14 by albagarc         ###   ########.fr       */
+/*   Updated: 2022/12/21 12:31:50 by albagarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../inc/defines.h"
@@ -14,42 +14,40 @@
 #include <stddef.h>
 #include <stdio.h>
 
-void    print_list1 (t_element **list_a)
+void	print_list1(t_element **list_a)
 {
-	t_element *temp_a;
- 
-	temp_a = *list_a;
+	t_element	*temp_a;
 
+	temp_a = *list_a;
 	while (temp_a)
 	{
 		temp_a = temp_a->next;
 	}
 }
 
-int min_index_in_stack(t_element **stack)
+int	min_index_in_stack(t_element **stack)
 {
-	t_element *temp;
-	int i;
-	
+	t_element	*temp;
+	int			i;
+
 	temp = *stack;
-	i= temp->index;
-	while(temp)
+	i = temp->index;
+	while (temp)
 	{
-		if(i > temp->index)
+		if (i > temp->index)
 			i = temp->index;
-		temp=temp->next;
+		temp = temp->next;
 	}
 	return (i);
-
 }
 
 int	is_not_sorted(t_element **stack)
 {
-	t_element *temp;
-	int i;
-	
+	t_element	*temp;
+	int			i;
+
 	temp = *stack;
-	i= min_index_in_stack(stack);
+	i = min_index_in_stack(stack);
 	while (temp)
 	{
 		if (temp->index == i)
@@ -58,65 +56,60 @@ int	is_not_sorted(t_element **stack)
 			temp = temp->next;
 		}
 		else
-			return(1);
+			return (1);
 	}
-	return(0);
+	return (0);
 }
 
-
-
-
-
-void	sort_3(t_element **stack, int length, int min_index)
+void	sort_3(t_element **stack, int length, int min_i)
 {
-	t_element *temp;
+	t_element	*t;
 
-	temp = *stack;
-	if(length == 2)
+	t = *stack;
+	if (length == 2)
 	{
-		if(temp->index > temp->next->index)
-			print_do_swap(stack, 0,'a', 0);
+		if (t->index > t->next->index)
+			print_do_swap(stack, 0, 'a', 0);
 		else
 			return ;
 	}
 	else
 	{
-		while(is_not_sorted(stack))
+		while (is_not_sorted(stack))
 		{
-			temp = *stack;
-			if((temp->index == min_index && temp->next->index == min_index + 2) || \
-				(temp->index == min_index + 2 && temp->next->index == min_index + 1) || \
-					(temp->index == min_index + 1 && temp->next->index == min_index))
+			t = *stack;
+			if ((t->index == min_i && t->next->index == min_i + 2) || \
+				(t->index == min_i + 2 && t->next->index == min_i + 1) || \
+					(t->index == min_i + 1 && t->next->index == min_i))
 				print_do_swap(stack, 0, 'a', 0);
-			else if(temp->index == min_index + 2 && temp->next->index == min_index)
+			else if (t->index == min_i + 2 && t->next->index == min_i)
 				print_do_rotate(stack, 0, 'a', 0);
-			else 
+			else
 				print_do_rrotate(stack, 0, 'a', 0);
 		}
 	}
 }
 
-
 //una funcion que decida si es min index o no
 int	is_min(int index)
 {
-	int min_index;
+	int	min_index;
 
 	min_index = 0;
-	if(index == min_index)
-			return(1);
+	if (index == min_index)
+		return (1);
 	else
-		return(0);	
+		return (0);
 }
-int is_next_min(int index)
+int	is_next_min(int index)
 {
-	int min_next_index;
+	int	min_next_index;
 
 	min_next_index = 1;
-	if(index == min_next_index)
-			return(1);
+	if (index == min_next_index)
+		return (1);
 	else
-		return(0);	
+		return (0);
 }
 
 int	lst_size(t_element *lst)
@@ -126,66 +119,64 @@ int	lst_size(t_element *lst)
 	i = 0;
 	while (lst != NULL)
 	{
-		lst = lst -> next;
+		lst = lst->next;
 		i++;
 	}
 	return (i);
 }
+
 void	sort_5_to_b(t_element **stack_1, t_element **stack_2, int length)
 {
-	t_element *temp_1;
-	t_element *last;
+	t_element	*t;
+	t_element	*last;
 
-	temp_1 = *stack_1;
-	last = lst_last(temp_1);
-	while(lst_size(temp_1) > 3)
+	t = *stack_1;
+	last = lst_last(t);
+	while (lst_size(t) > 3)
 	{
-		if(is_min(temp_1->index) || (is_next_min(temp_1->index) && length != 4))
-			print_do_push(stack_1,stack_2, 0, 'b');
-		else if(is_min(temp_1->next->index) || (is_next_min(temp_1->next->index) && length != 4))
-			{
-				print_do_swap(stack_1, 0, 'a', 0);
-				print_do_push(stack_1,stack_2, 0, 'b');
-			}
-		else if(is_min(last->index) || (is_next_min(last->index) && length != 4))
+		if (is_min(t->index) || (is_next_min(t->index) && length != 4))
+			print_do_push(stack_1, stack_2, 0, 'b');
+		else if (is_min(t->next->index) || (is_next_min(t->next->index) && length != 4))
+		{
+			print_do_swap(stack_1, 0, 'a', 0);
+			print_do_push(stack_1, stack_2, 0, 'b');
+		}
+		else if (is_min(last->index) || (is_next_min(last->index) && length != 4))
 		{
 			print_do_rrotate(stack_1, 0, 'a', 0);
-			print_do_push(stack_1,stack_2, 0, 'b');
+			print_do_push(stack_1, stack_2, 0, 'b');
 		}
 		else
-			print_do_rotate(stack_1, 0, 'a', 0);	
-		temp_1 = *stack_1;
-		last = lst_last(temp_1);
+			print_do_rotate(stack_1, 0, 'a', 0);
+		t = *stack_1;
+		last = lst_last(t);
 	}
 }
 
 void	sort_5(t_element **stack_1, t_element **stack_2, int length)
 {
-	t_element *temp_2;
-	int min_index;
-	
+	t_element	*temp_2;
+	int			min_index;
+
 	temp_2 = *stack_2;
-	if(!is_not_sorted(stack_1))
+	if (!is_not_sorted(stack_1))
 		return ;
-	else
+	sort_5_to_b(stack_1, stack_2, length);
+	min_index = min_index_in_stack(stack_1);
+	sort_3(stack_1, length - 2, min_index);
+	temp_2 = *stack_2;
+	if (temp_2->index == 0)
 	{
-		sort_5_to_b(stack_1, stack_2, length);
-		min_index = min_index_in_stack(stack_1);
-		sort_3(stack_1, length - 2, min_index);
-		temp_2 = *stack_2;
-		if(temp_2->index == 0)
+		if (lst_size(temp_2) == 2)
 		{
-			if(lst_size(temp_2) == 2)
-				{
-					print_do_swap(0, stack_2, 0, 'b');
-					print_do_push(stack_1,stack_2,'a',0);
-				}
-			print_do_push(stack_1,stack_2,'a',0);
+			print_do_swap(0, stack_2, 0, 'b');
+			print_do_push(stack_1, stack_2, 'a', 0);
 		}
-		if(temp_2->index == 1)
-		{
-			print_do_push(stack_1,stack_2,'a',0);
-			print_do_push(stack_1,stack_2,'a',0);
-		}
+		print_do_push(stack_1, stack_2, 'a', 0);
+	}
+	if (temp_2->index == 1)
+	{
+		print_do_push(stack_1, stack_2, 'a', 0);
+		print_do_push(stack_1, stack_2, 'a', 0);
 	}
 }
