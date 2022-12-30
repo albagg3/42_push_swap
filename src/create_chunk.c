44993348dd17ce9c1ec6e9940b472e6df040a81b
albagg3 <garciagonzalezalba@gmail.com>
2022-12-30 18:57:18 +0100

    se mueven los chunks al stack b

--- src/create_chunk.c
+++ src/create_chunk.c
@@ -6,50 +6,79 @@
 /*   By: albagarc <albagarc@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 15:51:02 by albagarc          #+#    #+#             */
-/*   Updated: 2022/12/21 18:56:13 by albagarc         ###   ########.fr       */
+/*   Updated: 2022/12/30 18:56:38 by albagarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../inc/defines.h"
 #include "../inc/utils.h"
 
-void	create_stack_b(t_element **stack_a,t_element **stack_b, int length)
-{
-	t_element	*t;
-	int			i;
-//	int			min_index;
+//si el numero es parte de un chunk lo pushea a la stack_b
 
-	i = 0;
-	t = *stack_a;
-	 while (t)
-	 {
-//		min_index = min_index_in_stack(stack_a); 
-		if (t->index >= 0 && t->index <= 10)
+int	is_chunk(t_element **stack_a, t_element **stack_b, int length, int i)
+{
+	if(i < length / 2)
+	{
+		while(i)
 		{
-//			create_chunk();
-			if (i < length / 2)
-			{
-				while(i)
-				{
-					print_do_rotate(stack_a, 0, 'a', 0);
-					i--;
-				}
-			}
-			if (i > length / 2)
-			{
-				while(i < length)
-				{
-					print_do_rrotate(stack_a, 0, 'a', 0);
-					i++;
-				}
-			}
-			print_do_push(stack_a, stack_b, 0, 'b');
-			length--;
-			t = *stack_a;
+			print_do_rotate(stack_a, 0, 'a', 0);
+			i--;
 		}
-		i++;
-		t = t->next;
-	 }
+	}
+	if (i > length / 2)
+	{
+		while(i < length)
+		{
+			print_do_rrotate(stack_a, 0, 'a', 0);
+			i++;
+		}
+	}
+	print_do_push(stack_a, stack_b, 0, 'b');
+	//despues de pushear analizamos stack_b
+	length--;
+ 	return length;	
 }
 
+int	min_chunk(t_element **stack_b, int len)
+{
+	int	min_in_chunk;
+	int 	n;
+	
+	min_in_chunk = len / 4;
+	if(!*stack_b || lst_size(*stack_b) < len / 4)
+		min_in_chunk = 0;
+	else
+	{
+		n = lst_size(*stack_b) / (len / 4);
+		min_in_chunk *= n;
+	}
+	return (min_in_chunk);
+}
+
+void	create_stack_b(t_element **stack_a,t_element **stack_b, int len)
+{
+	t_element	*t;
+	int		i;
+	int 		min_in_chunk;
+	int		new_len;
+	
+	i = 0;
+	t = *stack_a;
+	new_len = len;
+	 while (t)
+	 {
+		min_in_chunk = min_chunk(stack_b, len);
+		if (t->index >= min_in_chunk && t->index < min_in_chunk + len / 4)
+		{
+			new_len = is_chunk(stack_a, stack_b, new_len, i);
+			t = *stack_a;
+			i = 0;
+		}
+		else
+		{
+			i++;
+			t = t->next;
+		}
+	 }
+}
 
 
